f2f8d554ba065e786267d8ee2484e82e1dd516bd
leonorlima <leonorslima@ua.pt>
2020-06-23 15:37:54 +0100

    socorro

--- public/interacoes_stepper2.js
+++ public/interacoes_stepper2.js
@@ -2,7 +2,7 @@ const previousBtn = document.getElementById('previousBtn');
 const nextBtn = document.getElementById('nextBtn');
 const finishBtn = document.getElementById('finishBtn');
 const content = document.getElementById('content');
-const bullets = [...document.querySelectorAll('.bullet')];
+const bullets = [ ... document.querySelectorAll('.bullet')];
 
 const MAX_STEPS = 3;
 let currentStep = 1;
@@ -18,3 +18,37 @@ nextBtn.addEventListener('click', () => {
     }
     content.innerText = `Step number ${currentStep}`;
 });
+
+previousBtn.addEventListener('click', () => {
+    const previousBullet = bullets[currentStep - 2];
+    previousBullet.classList.remove('completed');
+    currentStep--;
+    nextBtn.disabled = false;
+    finishBtn.disabled = true;
+    if (currentStep === 1) {
+        previousBtn.disabled = true;
+    }
+    content.innerText = `Step number ${currentStep}`;
+});
+
+finishBtn.addEventListener('click', () => {
+    location.reload();
+});
+
+/*
+if(currentStep === 1) {
+    document.getElementById("pag2").style.visibility = "hidden";
+    document.getElementById("pag3").style.visibility = "hidden";
+}
+
+if(currentStep === 2) {
+    document.getElementById("pag1").style.visibility = "hidden";
+    document.getElementById("pag3").style.visibility = "hidden";
+}
+
+if(currentStep === 3) {
+    document.getElementById("pag1").style.visibility = "hidden";
+    document.getElementById("pag2").style.visibility = "hidden";
+}
+*/
+
