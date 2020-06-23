bea5085c678ef69db0588fdeb7abf03445f40c52
leonorlima <leonorslima@ua.pt>
2020-06-23 14:49:16 +0100

    socorro

---
+++ public/interacoes_stepper2.js
@@ -0,0 +1,20 @@
+const previousBtn = document.getElementById('previousBtn');
+const nextBtn = document.getElementById('nextBtn');
+const finishBtn = document.getElementById('finishBtn');
+const content = document.getElementById('content');
+const bullets = [...document.querySelectorAll('.bullet')];
+
+const MAX_STEPS = 3;
+let currentStep = 1;
+
+nextBtn.addEventListener('click', () => {
+    const currentBullet = bullets[currentStep - 1];
+    currentBullet.classList.add('completed');
+    currentStep++;
+    previousBtn.disabled = false;
+    if(currentStep === MAX_STEPS){
+        nextBtn.disabled = true;
+        finishBtn.disabled = false;
+    }
+    content.innerText = `Step number ${currentStep}`;
+});
