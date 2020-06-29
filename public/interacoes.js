4ccea6f5aeccd6b5fad17f36d01250fa62956d42
leonorlima <leonorslima@ua.pt>
2020-06-29 12:36:52 +0100

    socorro

--- public/interacoes.js
+++ public/interacoes.js
@@ -7,5 +7,11 @@ function off() {
     document.getElementById("overlay").style.display = "none";
 }
 
+function abrir() {
+    document.getElementById("caixa").style.height = "500px";
+    document.getElementById("comentarios").style.visibility = "visible";
+
+}
+
 
 
