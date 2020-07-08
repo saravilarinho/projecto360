e0b8e753370ea35c14c08dd5c039f3deda590298
sara <ssvilarinho@ua.pt>
2020-07-08 15:20:34 +0100

    redirecionar

--- public/index.php
+++ public/index.php
@@ -1,3 +1,48 @@
-<?php
-header("Location: login.php");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <!-- JS, Popper.js, and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+
+    <!-- Fonte -->
+    <link href="https://fonts.googleapis.com/css2?family=Muli&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Muli:wght@400;500&display=swap" rel="stylesheet">
+
+    <!-- Ligacoes -->
+    <script src="interacoes.js" ></script>
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+    <script src="https://kit.fontawesome.com/2a97b08cd6.js" crossorigin="anonymous"></script>
+
+    <title>Inicio</title>
+</head>
+<body class="container">
+<div class="row justify-content-center inicio">
+    <img class="col-6 col-lg-3 align-self-center mt-5" src="imagens/logocslogan.png">
+</div>
+
+
+<div class="justify-content-center d-flex mt-5">
+
+<a href="login.php" style="width: 100%; text-align: center;">
+    <button type="button" class="btn botao_grande">
+        Entrar
+    </button>
+    </a>
+</div>
+
+<div class="justify-content-center d-flex mt-3">
+    <a href="registo.php" style="width: 100%; text-align: center;">
+    <button type="button" class="btn botao_grande">
+        Registar
+    </button>
+    </a>
+</div>
+</body>
+</html>
